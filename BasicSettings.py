7fb813571927c70cf60e71bcee28142c3fe99bda
JannisKokkinis <71180363+JannisKokkinis@users.noreply.github.com>
2022-05-27 11:31:04 +0200

    Update BasicSettings.py

--- BasicSettings.py
+++ BasicSettings.py
@@ -1,7 +1,6 @@
 lang = 'de'
 content = 'Sehr geehrte Damen und Herren der Geschäftsleitung der SBB, Hiermit beschwere ich mich zum 5 Male schriftlich, dass die Zugverbindungen von Bern nach Basel (namentlich die Züge ab 17:36 und 18:04 immer verspätet ankommen! Die letzten Male mit derartigen Verspätungen, dass ich nun innert 2 Wochen zum 5ten Male mein Anschlusszug nach Aesch verpasst habe. Beim Nachfragen des Zugspersonals konnte mir niemand einen Grund nennen, weshalb die Verspätung zu Stande kam. Auch vermisse ich eine Reaktion seitens des Beschwerdemanagement der SBB!  Ich erwarte eine Begründung für die genannten Verspätungen und die konkreten Massnahmen, was Sie gedenken gegen die Verspätungen zu tun. Freundliche Grüsse Heinz Müller'
 
-#Für GitHub
 MicrosoftKey = 'enteryourkeyhere'
 MicrosoftEndpoint = 'https://yoururl.cognitiveservices.azure.com/'
 
